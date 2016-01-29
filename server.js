f1b656fd709d5a236dce35821e5800a18823806b
AJ <Me>
2016-01-29 09:50:06 -0600

    Posting in the APP

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import {MongoClient} from 'mongodb';
 
 let app = express();
+let db;
 
 const PORT = 9005;
 const MONGO_URL = 'mongodb://engls:3phf084hf023h0gwedfrd32s@ds051615.mongolab.com:51615/react-relay-graphql';
@@ -9,19 +10,15 @@ const MONGO_URL = 'mongodb://engls:3phf084hf023h0gwedfrd32s@ds051615.mongolab.co
 //app.get('/', (req,res) => res.send('This is express ;)'));
 app.use(express.static('public'));
 
-app.listen(PORT, () => console.log('Listening on port ' + PORT));
+MongoClient.connect(MONGO_URL, (err, database) => {
+  if(err) { throw err; }
+  db = database;
+  app.listen(PORT, () => console.log('Listening on port ' + PORT));
+});
 
-MongoClient.connect(MONGO_URL, (err, db) => {
-  if(err) {
-    console.log('Its not connected');
-    throw err;
-  }
-  console.log("Connected correctly to server");
-
+app.get('/data/links', (req, res) => {
   db.collection('links').find({}).toArray((err, docs) => {
-    if(err) {
-      throw err;
-    }
-    console.log(docs);
+    if(err) { throw err; }
+    res.json(docs);
   });
 });
